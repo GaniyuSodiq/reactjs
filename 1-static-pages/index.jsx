1179f57339b02a4bb96edac4a5c485537547842e
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-18 05:44:09 +0100

    wrote my first few lines of react. Hurray

---
+++ 1-static-pages/index.jsx
@@ -0,0 +1,37 @@
+// to setup the most basic react app,
+// you need to do 2 things
+
+// 1- create a root
+// 2- render some markup to the root
+
+// to create a root, 
+// i need to import a method called root from the react DOM library from a sub module called reactClient
+import {createRoot} from "react-dom/client"
+
+// we then use the method to create a root
+// what we need to pass to the createRoot method is the dom node in our index.html
+const root = createRoot(document.getElementById("root"))
+
+// why is it called 'root'
+// since we are going to be rendering all of our markup using react in our javascript files,
+// we need one central location where react can insert all of that content
+// the div with the id of root that we setup in index.html, 
+// will be the root of our application
+// it is the one place where react will insert everything
+// what we need to pass to the createRoot method is the dom node in our index.html
+
+// now that we have root in our js, 
+// i can call a method on that root, a method called render
+// and pass in what looks alot like normal html
+root.render(<h1>Hello, React!</h1>)
+
+// CHALLENGE - rewrite the first 3 react code we have above this sentence
+
+// import {createRoot} from "react-dom/client"
+// const root = createRoot(document.getElementById("root"))
+// root.render(<h4>Wowo</h4>)
+
+// you might see a similar way in the wild. which is the same thing.
+
+// import ReactDOM from "react-dom/client"
+// ReactDOM.createRoot(document.getElementById("root")).render(<h4>Wowo</h4>)
