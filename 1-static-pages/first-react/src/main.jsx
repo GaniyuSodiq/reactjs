4c6366e7a225d088cf447d10ad88d8ab5440af14
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-22 08:05:07 +0100

    the first static page done

--- 1-static-pages/first-react/src/main.jsx
+++ 1-static-pages/first-react/src/main.jsx
@@ -1,21 +1,22 @@
-import { createRoot } from "react-dom/client"
-
+// FIRST PROJECT
+import './index.css'
+import { createRoot } from 'react-dom/client'
 const root = createRoot(document.getElementById("root"))
-
 root.render(
   <main>
-    <img src="src/assets/react.svg"></img>
-    <h1>Welcome, React</h1>
+    <img src="src/assets/react.svg" alt="" /><h1>Fun Facts About React</h1>
+    <div className='content'>
+      <div className='lists'>
+        <ul>
+          <li>React is a library, not a framework</li>
+          <li>Born out of Facebook's need for a better UI</li>
+          <li>Powers more than just web apps</li>
+          <li>Loved by industry giants</li>
+        </ul>
+      </div>
+      <div>
+        <img src="src/assets/react.svg" alt="" className='img-right'/>
+      </div>
+    </div>
   </main>
 )
-
-// with static images that aren't going to be changing or coming froma CDN or database,
-// there is a unique way to deal with them in react
-
-// in vanilla js html img is called like like this: <img src = "assets/react.svg">
-// bcs the code file is in the same location as the assets folder
-// it is somehow different here and it can be a challenge in reactjs, 
-// a quick fix is to use absolute path like in the .render()
-// <img src="src/assets/react.svg"></img>
-
-// JSX expression must have one parent element.
--- 1-static-pages/index.jsx
+++ 1-static-pages/index.jsx
@@ -210,7 +210,7 @@ root1.render(
 
     in imperative, we have to tell the computer what to do every single step
     like the code below
- */
+*/
 
     const root = document.getElementById("root")
 
@@ -222,3 +222,29 @@ root1.render(
 
     // we dont have to do all this in React.
 
+
+/** ANOTHER CHALLENGE
+
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <main>
+    <img src="src/assets/react.svg"></img>
+    <h1>Welcome, React</h1>
+  </main>
+)
+
+*/
+
+// with static images that aren't going to be changing or coming froma CDN or database,
+// there is a unique way to deal with them in react
+
+// in vanilla js html img is called like like this: <img src = "assets/react.svg">
+// bcs the code file is in the same location as the assets folder
+// it is somehow different here and it can be a challenge in reactjs, 
+// a quick fix is to use absolute path like in the .render()
+// <img src="src/assets/react.svg"></img>
+
+// JSX expression must have one parent element.
