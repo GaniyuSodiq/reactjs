787044d0acca061e6c5e20b7cc4835cb656d2a26
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-23 04:53:13 +0100

    React Fragment

--- 1-static-pages/first-react/src/main.jsx
+++ 1-static-pages/first-react/src/main.jsx
@@ -49,33 +49,33 @@ root.render(
 */
 
 // CHALLENGE - create a Page component. List why u learn react
-function Page() {
-    return (
-        <div>
-            <header>
-                <img src="src/assets/react.svg" alt="React Logo" />
-            </header>
-            <main>
-                <h1>Reasons I'm Excited To Learn React</h1>
-                <ol>
-                    <li>I WANT TO LEARN REACT NATIVE</li>
-                    <li>I NEED A BETTER INCOME</li>
-                    <li>I WANT TO CREATE SOFTWARE ENGINEERING COMPANY</li>
-                </ol>
-            </main>
-            <footer>
-                <small>c 2025 <span>Ganiyu</span> Developement. All rights reserved</small>
-            </footer>
-        </div>
-    )
-}
+// function Page() {
+//     return (
+//         <div>
+//             <header>
+//                 <img src="src/assets/react.svg" alt="React Logo" />
+//             </header>
+//             <main>
+//                 <h1>Reasons I'm Excited To Learn React</h1>
+//                 <ol>
+//                     <li>I WANT TO LEARN REACT NATIVE</li>
+//                     <li>I NEED A BETTER INCOME</li>
+//                     <li>I WANT TO CREATE SOFTWARE ENGINEERING COMPANY</li>
+//                 </ol>
+//             </main>
+//             <footer>
+//                 <small>c 2025 <span>Ganiyu</span> Developement. All rights reserved</small>
+//             </footer>
+//         </div>
+//     )
+// }
 
 
-root.render(
-        <Page />
-        /* components can also be written in opening and closing bracket
-        <Page>Some content can be here</Page> */ 
-)
+// root.render(
+//         <Page />
+//         /* components can also be written in opening and closing bracket
+//         <Page>Some content can be here</Page> */ 
+// )
 
 
 
@@ -105,3 +105,83 @@ root.render(
 //         <Footer />
 //     </>
 // )
+
+
+
+
+
+// ❤️‍🔥 FRAGMENT - a built-in component from React
+// we first import it
+
+import { Fragment } from 'react'
+
+// we can render it as if it were a regular html element
+
+// function Page() {
+//     return (
+//         <Fragment>
+//             <header>
+//                 <img src="src/assets/react.svg" alt="React Logo" />
+//             </header>
+//             <main>
+//                 <h1>Reasons I'm Excited To Learn React</h1>
+//                 <ol>
+//                     <li>I WANT TO LEARN REACT NATIVE</li>
+//                     <li>I NEED A BETTER INCOME</li>
+//                     <li>I WANT TO CREATE SOFTWARE ENGINEERING COMPANY</li>
+//                 </ol>
+//             </main>
+//             <footer>
+//                 <small>c 2025 <span>Ganiyu</span> Developement. All rights reserved</small>
+//             </footer>
+//         </Fragment>
+//     )
+// }
+
+// root.render(
+//         <Page />
+// )
+
+/**
+ * There is a little difference in using Fragments compare to using div to house our html tags
+ * 
+ * When we used a div - remeber the div renders inside the <div id="root"> </div> in our html
+ * So what we end up getting is another div inside the html root div
+ * 
+ * When we use a Fragment, React will not insert another level of element
+ * Fragment just allows us to have multiple html node inside of our .render 
+ * without us worrying about an unnecessary node in our html
+ * 
+ * And using Fragment has been simplified 
+ * we can just use '<> </>' tags and disregard the import too
+ */
+
+function Page() {
+    return (
+        <>
+            <header>
+                <img src="src/assets/react.svg" alt="React Logo" />
+            </header>
+            <main>
+                <h1>Reasons I'm Excited To Learn React</h1>
+                <ol>
+                    <li>I WANT TO LEARN REACT NATIVE</li>
+                    <li>I NEED A BETTER INCOME</li>
+                    <li>I WANT TO CREATE SOFTWARE ENGINEERING COMPANY</li>
+                </ol>
+            </main>
+            <footer>
+                <small>c 2025 <span>Ganiyu</span> Developement. All rights reserved</small>
+            </footer>
+        </>
+    )
+}
+
+/**
+ * So you can see this empty tags in react 
+ * know that this allows us to render sibling react element
+ */
+
+root.render(
+        <Page />
+)
