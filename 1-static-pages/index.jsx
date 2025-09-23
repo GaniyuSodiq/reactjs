3be858106b8bae83ac84019a23c8daae216ce84a
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-23 04:25:17 +0100

    React custom components

--- 1-static-pages/index.jsx
+++ 1-static-pages/index.jsx
@@ -248,3 +248,109 @@ root.render(
 // <img src="src/assets/react.svg"></img>
 
 // JSX expression must have one parent element.
+
+
+/** // FIRST PROJECT - my code - SAME AS TUTOR'S CODE
+import './index.css'
+import { createRoot } from 'react-dom/client'
+const root = createRoot(document.getElementById("root"))
+root.render(
+  <main>
+    <img src="src/assets/react.svg" alt="" /><h1>Fun Facts About React</h1>
+    <div className='content'>
+      <div className='lists'>
+        <ul>
+          <li>React is a library, not a framework</li>
+          <li>Born out of Facebook's need for a better UI</li>
+          <li>Powers more than just web apps</li>
+          <li>Loved by industry giants</li>
+        </ul>
+      </div>
+      <div>
+        <img src="src/assets/react.svg" alt="" className='img-right'/>
+      </div>
+    </div>
+  </main>
+)
+
+
+You wouldnt put all code inside of .render() in actual projects
+you would use something called custom components
+*/
+
+
+
+// ❤️‍🔥 MAKING CUSTOM COMPONENT IN REACT
+
+// lets say we want to run our jsx from from a function
+
+// function temporaryName() { instead of camelCase, components use PascalCase
+function TemporaryName() {
+    // we can just start the jsx at the fron of return keyword, hwever we can put jsx in () too
+    return (
+        <main>
+            <img src="src/assets/react.svg" alt="" /><h1>Fun Facts About React</h1>
+            <div className='content'>
+                <div className='lists'>
+                    <ul>
+                        <li>React is a library, not a framework</li>
+                        <li>Born out of Facebook's need for a better UI</li>
+                        <li>Powers more than just web apps</li>
+                        <li>Loved by industry giants</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="src/assets/react.svg" alt="" className='img-right' />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+/**
+root.render(
+  temporaryName() // this works the same way as before
+  // and it make sence bcs this function returns the jsx we need for our .render()
+  // but this is not exactly how we use components in react
+)
+*/
+
+// Components are written in Pascal Case: temporaryName will be TemporaryName
+// And instead of calling it with brackets TemporaryName(), 
+// we call it by putting in self closing html angle bracket <TemporaryName />
+/**
+
+root.render(
+    <TemporaryName />
+)
+
+*/
+
+// CHALLENGE - create a Page component. List why u learn react
+function Page() {
+    return (
+        <div>
+            <header>
+                <img src="src/assets/react.svg" alt="React Logo" />
+            </header>
+            <main>
+                <h1>Reasons I'm Excited To Learn React</h1>
+                <ol>
+                    <li>I WANT TO LEARN REACT NATIVE</li>
+                    <li>I NEED A BETTER INCOME</li>
+                    <li>I WANT TO CREATE SOFTWARE ENGINEERING COMPANY</li>
+                </ol>
+            </main>
+            <footer>
+                <small>c 2025 <span>Ganiyu</span> Developement. All rights reserved</small>
+            </footer>
+        </div>
+    )
+}
+
+
+root.render(
+        <Page />
+        /* components can also be written in opening and closing bracket
+        <Page>Some content can be here</Page> */ 
+)
