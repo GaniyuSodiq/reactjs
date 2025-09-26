3f82a3bfe9f4828ed485512a796305653e451bda
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-26 18:17:16 +0100

    styled the header

--- 1-static-pages/funfact1/src/components/Header.jsx
+++ 1-static-pages/funfact1/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import '../index.css'
-export default function Header(){
+export default function Header() {
     return (
         <header>
-            <img src="src/assets/react.svg" alt="react logo" />
-            <span>ReactFacts</span>
+            <div className='container' id='logo'>
+                <img src="src/assets/react.svg" alt="react logo" />
+                <span>ReactFacts</span>
+            </div>
         </header>
     )
 }
