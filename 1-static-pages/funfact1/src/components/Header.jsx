b11d7f9fee1a794caf239dc76dd3db5aa9a50853
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-26 17:50:00 +0100

    created the components

---
+++ 1-static-pages/funfact1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../index.css'
+export default function Header(){
+    return (
+        <header>
+            <img src="src/assets/react.svg" alt="react logo" />
+            <span>ReactFacts</span>
+        </header>
+    )
+}
