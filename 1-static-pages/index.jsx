4e55c09001dfa43e7bde4028248fc83ce772109e
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-30 02:58:57 +0100

    props fully explained

--- 1-static-pages/index.jsx
+++ 1-static-pages/index.jsx
@@ -1208,3 +1208,71 @@ import generalImage from "./assets/react.svg"
 
 
 */
+
+// ❤️‍🔥 Pass props to Entry component
+
+/**
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Entry
+        img={
+          {
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            alt: "Mount Fuji"
+          }
+        }
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      />
+    </>
+  )
+}
+
+export default function Entry(props){
+    console.log(props)
+    return (
+        <section className="container">
+            <div className="main-image-container">
+                <img className="main-image" src={props.img.src} alt={props.img.alt} />
+            </div>
+
+            <div className="location-details">
+                <div className="location-address">
+                    <span className="country">📌{props.country}</span>
+                    <a href={props.googleMapsLink}>View on Google Maps</a>
+                </div>
+                <h1 className="location-name">
+                    {props.title}
+                </h1>
+                <h4 className="date-visited">
+                    {props.dates}
+                </h4>
+                <p className="details">
+                    {props.text}
+                </p>
+            </div>
+        </section>
+    )
+}
+
+NOW we know that are data is not hardcoded into the component
+instead it is being pulledin from the outside 
+
+The above code means we have the power to duplcate Entry components with different data
+not just Japan with Mount Fuji
+
+*/
+
+
+// ❤️‍🔥 Reviewing array .map()
+
+/**
+We are going to see how to use js array .map method to take data and turn it into jsx
+
+
+*/
---
+++ 2-data-driven/travel-journal/src/components/Entry.jsx
@@ -0,0 +1,26 @@
+export default function Entry(props){
+    console.log(props)
+    return (
+        <section className="container">
+            <div className="main-image-container">
+                <img className="main-image" src={props.img.src} alt={props.img.alt} />
+            </div>
+
+            <div className="location-details">
+                <div className="location-address">
+                    <span className="country">📌{props.country}</span>
+                    <a href={props.googleMapsLink}>View on Google Maps</a>
+                </div>
+                <h1 className="location-name">
+                    {props.title}
+                </h1>
+                <h4 className="date-visited">
+                    {props.dates}
+                </h4>
+                <p className="details">
+                    {props.text}
+                </p>
+            </div>
+        </section>
+    )
+}
