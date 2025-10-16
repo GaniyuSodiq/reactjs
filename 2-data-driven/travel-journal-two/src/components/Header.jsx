25448146915d8efb6cbd9344af0c87c81c212885
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-10-16 15:16:29 +0100

    implement header to the design

---
+++ 2-data-driven/travel-journal-two/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return <header>
+        <div className="header-box">
+            <span>🌍</span>
+            <span> my travel journal </span>
+        </div>
+    </header>
+}
