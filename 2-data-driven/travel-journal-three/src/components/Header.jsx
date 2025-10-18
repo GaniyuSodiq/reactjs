b971211463c5e104c2a3f1f9e0072a572f802b5d
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-10-18 09:07:21 +0100

    started third journal travel

---
+++ 2-data-driven/travel-journal-three/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="header-text">
+                <span>🌐</span>
+                <h1>my travel journal three</h1>
+            </div>
+        </header>
+    )
+}
