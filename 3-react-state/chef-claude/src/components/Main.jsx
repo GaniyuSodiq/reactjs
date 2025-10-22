77ae029f0aa1e1f9f88d60dccbebac8ec681d5f5
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-10-22 19:56:55 +0100

    main

---
+++ 3-react-state/chef-claude/src/components/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main(){
+    return(
+        <main>
+            <form action="" >
+                <label htmlFor="addIngredient" className="add-ingredient-form">
+                    <input type="text" placeholder="e.g oregano" aria-label="Add ingredient" id="addIngredient"/>
+                    <button>Add Ingredient</button>
+                </label>
+            </form>
+        </main>
+    )
+}
