dc4202f6734029fd852d6ab37d7c286a75c0e2cb
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-10-23 08:12:46 +0100

    recieve input

--- 3-react-state/EventListener/src/App.jsx
+++ 3-react-state/EventListener/src/App.jsx
@@ -1,9 +1,21 @@
 export default function App() {
+
+  function handleClick() {
+    alert("hello Sodiq")
+  }
+
+  function handleMouseOver() {
+    alert("Moused over")
+  }
+
   return (
     <main className="container">
-      <img src="https://picsum.photos/640/360"
-        alt="Placeholder image from Picsum" />
-      <button>Click Me</button>
+      <img
+        onMouseOver={handleMouseOver}
+        src="https://picsum.photos/640/360"
+        alt="Placeholder image from Picsum"
+      />
+      <button onClick={handleClick}>Click Me</button>
     </main>
   )
 }
--- 3-react-state/chef-claude/src/components/Main.jsx
+++ 3-react-state/chef-claude/src/components/Main.jsx
@@ -1,12 +1,32 @@
-export default function Main(){
-    return(
+export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const mappedIngredients = ingredients.map(ingr => <li key={ingr}>{ingr}</li>)
+
+    function handleOnSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        alert(newIngredient)
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
+    return (
         <main>
-            <form action="" >
+            <form action="" onSubmit={handleOnSubmit}>
                 <label htmlFor="addIngredient" className="add-ingredient-form">
-                    <input type="text" placeholder="e.g oregano" aria-label="Add ingredient" id="addIngredient"/>
+                    <input
+                        type="text"
+                        placeholder="e.g oregano"
+                        aria-label="Add ingredient"
+                        id="addIngredient"
+                        name="ingredient" 
+                    />
                     <button>Add Ingredient</button>
                 </label>
             </form>
+            <ul>
+                {mappedIngredients}
+            </ul>
         </main>
     )
 }
