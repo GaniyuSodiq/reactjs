deefae31def05f153c192f61c944b31575e87b36
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-11-30 09:21:15 +0100

    started chef claude project

--- 3-react-state/chef-claude/src/components/Header.jsx
+++ 3-react-state/chef-claude/src/components/Header.jsx
@@ -1,12 +1,10 @@
-import headerImage from "../images/chef-claude-icon.png"
+import ClaudeImage from "./../images/chef-claude-icon.png"
 
 export default function Header(){
     return(
-        <header>
-            <div className="header-content">
-                <img src={headerImage} alt="header-image" width="40px" height="50px"/>
-                <h1>Chef Claude</h1>
-            </div>
+        <header className="">
+            <img src={ClaudeImage} />
+            <h1>Chef Claude</h1>
         </header>
     )
 }
--- 3-react-state/chef-claude/src/main.jsx
+++ 3-react-state/chef-claude/src/main.jsx
@@ -1,9 +1,8 @@
-import { createRoot } from "react-dom/client";
 import App from "./App"
 import "./index.css"
-
+import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"))
-
+ 
 root.render(
   <App />
 )
