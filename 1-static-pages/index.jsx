f50dc450e0e4938152e3f94ab57c50e61e5fedbb
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-12-03 08:50:12 +0100

    ternary operator

--- 1-static-pages/index.jsx
+++ 1-static-pages/index.jsx
@@ -2196,3 +2196,57 @@ export default function App() {
     )
 }
 */
+
+//❤️‍🔥 REVIEW OF TERNARY OPERATOR IN JAVASCRIPT
+
+// export default function App() {
+  /**
+   * Challenge: Replace the if/else below with a ternary
+   * to determine the text that should display on the page
+   */
+  // const isGoingOut = true
+
+  // let answer  // 👈 Use ternary here
+
+  // let answer = isGoingOut ? "Yes" : "No"
+
+  // Remove the code below 👇 once your ternary is done
+  // if(isGoingOut === true) {
+  //     answer = "Yes"
+  // } else {
+  //     answer = "No"
+  // }
+
+  /**
+   * Ternary operator is just a way to have if/else statement on a single line
+   * let answer = isGoingOut === true ? "Yes" : "No"
+   * Ternary operator is simply asking a question: 
+   * is isGoingOut true ? return "Yes" if true and return "No" if false
+   * it can be written as 'const answer = isGoingOut ? "Yes" : "No"
+   * we change to const bcs we are not in the if else scope anymore
+   * and the isGoingOut is alreadya bollean so no need to equate it to true, 
+   * String is already truthy by default
+   * 
+   * Note that we we are rendering the result as answer in our component.
+   * there is another way to do this and that is to move the ternary into my component.
+  */
+
+  /**
+   * Challenge: move the Ternary directly inside the JSX
+  */
+/* 
+  const isGoingOut = true
+
+  /**
+   * so we no longer need answer variable
+   * we simple use what the ternary operator returns
+  
+
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button className="value">{isGoingOut ? "Yes" : "No"}</button>
+    </main>
+  )
+}
+*/
--- 3-react-state/useState/src/App.jsx
+++ 3-react-state/useState/src/App.jsx
@@ -1,91 +1,49 @@
-// HERE IS AN APP THAT COUNTS HOW MANY TIMES BOB IS GOING TO SAY STATE IN THIS ENTIRE SECTION
-
-import React from "react"
-
 export default function App() {
+  /**
+   * Challenge: Replace the if/else below with a ternary
+   * to determine the text that should display on the page
+   */
+  // const isGoingOut = true
 
-    const [count, setCount] = React.useState(0)
-   
-    /**
-     * in the body of the callback fn we need to return.. 
-     * the new value we want to state to be on the next render
-     * we could return count + 1, which is not different from what we had before
-     * the different is that this callback function will passed the oldValue of the state... 
-     * before your setCount ran  
-     * so with this I can use the oldValue in place of wht used to be count. this works!
-     *   
-    function add(){
-      setCount(function(oldValue){
-        return oldValue + 1
-      })
-    }
-    * And instead of using oldValue, we can use prev followed by the state variable name
-    * in the case count. so we use prevCount instead of oldValue
-    
-    function add(){
-      setCount(function(prevCount){
-        return prevCount + 1
-      })
-    }
-    * At first this might look like extra work but if we take the more easy sysntax of arrow fn
-    * we can simply say 
-    
-    function add(){
-      setCount(prevCount => prevCount + 1)
-    }
-    * and in the end it is not really different from what we had before
-    * BUT WHY DO WE HAVE TO DO THIS
-    * 
-    * Note: If you eve need the old value of state
-    * to help you determine the new value of state,
-    * you should pass a callback function to your 
-    * state setter function instead of using state directly.
-    * This callback function will recieve the old value 
-    * of state has its parameter, which you can then use
-    * to determine your new value of state.
-    * THIS IS A GENERAL RULE. 
-    * The issue of using state directly is nt something you will 
-    * come accross often but better safe than sorry. 
-    * And the syntax are even similar and simple.
-    * 
-    * lets see the issue with using the state directly
-    *     
-    function add(){
-      setCount(prevCount => prevCount + 1)
-      setCount(prevCount => prevCount + 1)
-      setCount(prevCount => prevCount + 1)
-    }
+  // let answer  // 👈 Use ternary here
 
-    function minus(){
-      setCount(count - 1)
-      setCount(count - 1)
-      setCount(count - 1)
-    }
-      the add function will add increament by 3 
-      and the minus will only decrease by 1
+  // let answer = isGoingOut ? "Yes" : "No"
 
-      you would expect the minus too to be by 3... but it doesnt work except in the callback fn
-    */
-    function add(){
-      setCount(prevCount => prevCount + 1)
-    }
+  // Remove the code below 👇 once your ternary is done
+  // if(isGoingOut === true) {
+  //     answer = "Yes"
+  // } else {
+  //     answer = "No"
+  // }
 
-    // function minus(){
-    //   setCount(count - 1)
-    // }
+  /**
+   * Ternary operator is just a way to have if/else statement on a single line
+   * let answer = isGoingOut === true ? "Yes" : "No"
+   * Ternary operator is simply asking a question: 
+   * is isGoingOut true ? return "Yes" if true and return "No" if false
+   * it can be written as 'const answer = isGoingOut ? "Yes" : "No"
+   * we change to const bcs we are not in the if else scope anymore
+   * and the isGoingOut is alreadya bollean so no need to equate it to true, 
+   * String is already truthy by default
+   * 
+   * Note that we we are rendering the result as answer in our component.
+   * there is another way to do this and that is to move the ternary into my component.
+  */
 
-    function minus(){
-      setCount(prevCount => prevCount -1)
-    }
+  /**
+   * Challenge: move the Ternary directly inside the JSX
+  */
+  const isGoingOut = true
 
-    return (
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button onClick={minus} className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">{count}</h2>
-                <button onClick={add} className="plus" aria-label="Increase count">+</button>
-            </div>
-        </main>
-    )
+  /**
+   * so we no longer need answer variable
+   * we simple use what the ternary operator returns
+  */
+
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button className="value">{isGoingOut ? "Yes" : "No"}</button>
+    </main>
+  )
 }
