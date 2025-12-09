e5693ce1f91912d9cb81070a4b528a05415e86f7
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-12-09 06:57:36 +0100

    finished form in React

--- 1-static-pages/index.jsx
+++ 1-static-pages/index.jsx
@@ -2805,7 +2805,8 @@ export default function App() {
     const password = formData.get("password")
     const employmentStatus = formData.get("employmentStatus")
     const dietaryRestrictions = formData.getAll("dietaryRestrictions")
-    console.log(dietaryRestrictions)
+    const favColor = formData.get("favColor")
+    console.log(favColor)
   }
 
   return (
@@ -2886,6 +2887,31 @@ export default function App() {
         </label>
         </fieldset>
 
+        {/* // Lets say we want to have a drop down where the user can chose their favorite color
+        // just select tags will give the option box and drop down arrow 
+        // options tags will make the options appear in the drop box  
+        // to get the options value in our funtion, we need to give the select a name
+        // the option that appears in the seletc box is by default the first on the list.. 
+        // in this example red.
+        // But we can set a defaultValue to what we want in the select tag
+        // there are some instances where what you see by default is an instruction
+        // Like: Choose a color
+        // we can achieve that by making an option with no value at the top.. 
+        // if we dont want our user to b abe to submit choose a color as an option
+        // we give the option 'disabled'*}
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select name="favColor" id="favColor">
+          <option value="" disabled>-- Choose a color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="indigo">Indigo</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="violet">Violet</option>
+        </select>
+
         <button>Submit</button>
 
       </form>
@@ -2894,3 +2920,120 @@ export default function App() {
 }
 */
 
+
+// ❤️‍🔥 Object.entriesFrom()
+/**
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function App() {
+  /*
+    function signUp(formData) {
+      const email = formData.get("email")
+      const password = formData.get("password")
+      const employmentStatus = formData.get("employmentStatus")
+      const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+      const favColor = formData.get("favColor")
+      console.log(favColor)
+    }
+  *
+  // currently we are going throgh our inputs and grabbing the info from them one after the other
+  // there are time when this is okay to do 
+  // but if you have a form with 40 different entries, then we need another way
+  // there is a utility built into javascript that we can use
+  // Objects.fromEntries
+  // we will pass in the entire formData object
+  // Object.fromEntries(formData)
+
+  function signUp(formData) {
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    // { email: 'joe@schmoe.com', password: 'password123', description: 'This is a description', 
+    //   employmentStatus: 'full-time', dietaryRestrictions: 'glutton-free', … }
+
+    // we get an object back. 
+    // the object conatins the names in the form as key and the names selected value as value.
+    // the error here is that the dietarRestrictions name is from a checkbox and multple values were selected
+    // but we are only getting a value returned in this object
+    // remember with checkboxes, we had to use formData.getAll(chebox-name)
+    // so we have to get chechboses data on its own
+
+    const dietaryData = formData.getAll("dietaryRestrictions")
+    console.log(dietaryData)
+    // ['vegan', 'glutton-free']
+
+    // now that we've got the array of the check options using getAll
+    // we can now replace them with what we have in Object.fromEntries at the chekbox place
+
+    const allData = {...data, dietaryRestrictions: dietaryData}
+    console.log(allData)
+    // {email: 'joe@schmoe.com', password: 'password123', description: 'This is a description', 
+    //  employmentStatus: 'full-time', dietaryRestrictions: Array(2), …}
+  }
+
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form action={signUp}>
+
+        <label htmlFor="email">Email:</label>
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" defaultChecked={true} value="full-time" />
+            Full-time
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Dietary Restrictions:</legend>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+            Kosher
+          </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="vegan" />
+            Vegan
+          </label>
+          <label>
+            <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="glutton-free" />
+            Glutton-free
+          </label>
+        </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select name="favColor" id="favColor">
+          <option value="" disabled>-- Choose a color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="indigo">Indigo</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="violet">Violet</option>
+        </select>
+
+        <button>Submit</button>
+
+      </form>
+    </section>
+  )
+}
+
+ */
--- 3-react-state/useState/src/App.jsx
+++ 3-react-state/useState/src/App.jsx
@@ -2,19 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export default function App() {
-  /**
-   * Challenge: grab the employment status from the form and log it
-   * to the console. (Remember to select one of the radios before submitting)
-   * 
-   * Note: This won't work the way you might expect quite yet!
-   */
+  /*
+    function signUp(formData) {
+      const email = formData.get("email")
+      const password = formData.get("password")
+      const employmentStatus = formData.get("employmentStatus")
+      const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+      const favColor = formData.get("favColor")
+      console.log(favColor)
+    }
+  */
+  // currently we are going throgh our inputs and grabbing the info from them one after the other
+  // there are time when this is okay to do 
+  // but if you have a form with 40 different entries, then we need another way
+  // there is a utility built into javascript that we can use
+  // Objects.fromEntries
+  // we will pass in the entire formData object
+  // Object.fromEntries(formData)
 
   function signUp(formData) {
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const employmentStatus = formData.get("employmentStatus")
-    const dietaryRestrictions = formData.getAll("dietaryRestrictions")
-    console.log(dietaryRestrictions)
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    // { email: 'joe@schmoe.com', password: 'password123', description: 'This is a description', 
+    //   employmentStatus: 'full-time', dietaryRestrictions: 'glutton-free', … }
+
+    // we get an object back. 
+    // the object conatins the names in the form as key and the names selected value as value.
+    // the error here is that the dietarRestrictions name is from a checkbox and multple values were selected
+    // but we are only getting a value returned in this object
+    // remember with checkboxes, we had to use formData.getAll(chebox-name)
+    // so we have to get chechboses data on its own
+
+    const dietaryData = formData.getAll("dietaryRestrictions")
+    console.log(dietaryData)
+    // ['vegan', 'glutton-free']
+
+    // now that we've got the array of the check options using getAll
+    // we can now replace them with what we have in Object.fromEntries at the chekbox place
+
+    const allData = {...data, dietaryRestrictions: dietaryData}
+    console.log(allData)
+    // {email: 'joe@schmoe.com', password: 'password123', description: 'This is a description', 
+    //  employmentStatus: 'full-time', dietaryRestrictions: Array(2), …}
   }
 
   return (
@@ -36,41 +65,44 @@ export default function App() {
           <label>
             <input type="radio" name="employmentStatus" value="unemployed" />
             Unemployed
-        </label>
+          </label>
           <label>
             <input type="radio" name="employmentStatus" value="part-time" />
             Part-time
-        </label>
+          </label>
           <label>
             <input type="radio" name="employmentStatus" defaultChecked={true} value="full-time" />
             Full-time
-        </label>
+          </label>
         </fieldset>
-
-        {/* // a very close relative to the radio button is checkbox
-        // with checkbox we can have multiple items checked at the same time
-        // which means we can also have more than one defaultChecked items
-        // one issue here is that when we select 2 values like checkbox allows
-        // our formData.get only icks the first value on the list 
-        // this is unique to checkbox and there is a simple way around it
-        // the .get method we are calling on formData.get also have .getAll
-        // .getAll gets the values and gives them to us in an array*/}
 
         <fieldset>
           <legend>Dietary Restrictions:</legend>
           <label>
             <input type="checkbox" name="dietaryRestrictions" value="kosher" />
             Kosher
-        </label>
+          </label>
           <label>
             <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="vegan" />
             Vegan
-        </label>
+          </label>
           <label>
             <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="glutton-free" />
             Glutton-free
-        </label>
+          </label>
         </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select name="favColor" id="favColor">
+          <option value="" disabled>-- Choose a color --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="indigo">Indigo</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="violet">Violet</option>
+        </select>
 
         <button>Submit</button>
 
