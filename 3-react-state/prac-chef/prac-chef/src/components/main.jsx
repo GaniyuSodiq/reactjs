ad41aacdfc8571fbbdea66ebe0553211f5ddaa42
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2026-01-17 11:02:07 +0100

    complete the input interactive part

--- 3-react-state/prac-chef/prac-chef/src/components/main.jsx
+++ 3-react-state/prac-chef/prac-chef/src/components/main.jsx
@@ -1,16 +1,44 @@
+import React from "react"
+
 export default function Main() {
+
+    const [ingredientArr, setIngredientArr] = React.useState(["Amala", "Gbegiri", "Ewedu"])
+
+    const ingredients = ingredientArr.map(ing => <li key={ing}>{ing}</li>)
+
+    function asubmitIngredient(formData) {
+        const newIngredient = formData.get("addIngredient")
+        setIngredientArr(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    function submitIngredient(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("addIngredient")
+        setIngredientArr(prevIngredients => [...prevIngredients, newIngredient])
+        event.currentTarget.reset()
+    }
+
     return (
         <>
             <main className="mainPage">
-                <form action="submitIngredient" className="addIngredientForm">
+                <form action="" onSubmit={submitIngredient} className="addIngredientForm">
                     <input
                         type="text"
                         aria-label="add ingredient"
-                        name="addIngredient" 
+                        name="addIngredient"
                         className="addIngredientInput"
                     />
                     <button className="addIngredientBtn">Add Ingredient</button>
                 </form>
+                <div className="ingredientsAtHand">
+                    <h3>Ingredients at hand:</h3>
+                    <ul>{ingredients}</ul>
+                    <div className="ctaBox">
+                        <p>Send the ingredient to Chef Simi to get the best recipe you could get</p>
+                        <button>Send To Chef</button>
+                    </div>
+                </div>
             </main>
         </>
     )
