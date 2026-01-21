438145f1e9e0c410ed26ff0ddc324df840b9921d
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2026-01-21 09:31:38 +0100

    used prettier on the QnA app

---
+++ 3-react-state/chef-aminat/chef-aminat/src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main(){
+    return (
+        <main>
+            <form action="">
+                <input type="text"
+                    aria-label="Add ingredient"
+                    placeholder="Maggi"
+                    name="ingredient"
+                />
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
--- 3-react-state/toggle-buttons3/QnA-app/src/Question.jsx
+++ 3-react-state/toggle-buttons3/QnA-app/src/Question.jsx
@@ -1,4 +1,18 @@
-import React from "react"
-export default function Question(props){
-    return <div onClick={props.handleSwitchQna} style={{backgroundColor: `${props.qn.color}`}}><p>{props.switchQnA ? `${'QUESTION: ' + props.qn.id}` : `${'ANSWER: ' + props.qn.id}`}</p> <span>{props.switchQnA ? `${props.qn.question}` : `${props.qn.answer}`}</span></div>
+import React from "react";
+export default function Question(props) {
+  return (
+    <div
+      onClick={props.handleSwitchQna}
+      style={{ backgroundColor: `${props.qn.color}` }}
+    >
+      <p>
+        {props.switchQnA
+          ? `${"QUESTION: " + props.qn.id}`
+          : `${"ANSWER: " + props.qn.id}`}
+      </p>
+      <span>
+        {props.switchQnA ? `${props.qn.question}` : `${props.qn.answer}`}
+      </span>
+    </div>
+  );
 }
