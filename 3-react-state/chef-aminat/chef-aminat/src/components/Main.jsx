292a3c480eb66136d2230460e01fbd58c7628d82
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2026-01-21 15:08:36 +0100

    create the form nd ingredient list

--- 3-react-state/chef-aminat/chef-aminat/src/components/Main.jsx
+++ 3-react-state/chef-aminat/chef-aminat/src/components/Main.jsx
@@ -1,14 +1,29 @@
-export default function Main(){
-    return (
-        <main>
-            <form action="">
-                <input type="text"
-                    aria-label="Add ingredient"
-                    placeholder="Maggi"
-                    name="ingredient"
-                />
-                <button>Add Ingredient</button>
-            </form>
-        </main>
-    )
+import React from "react";
+
+export default function Main() {
+    const [ingredientArr, setIngredientArr] = React.useState(["Maggi", "Rice"])
+    const ingredients = ingredientArr.map(ing => <li key={ing}>{ing}</li>)
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredientArr(prevIngredientArr => [...prevIngredientArr, newIngredient])
+    }
+  return (
+    <main>
+      <form action={addIngredient}>
+        <input
+          type="text"
+          aria-label="Add ingredient"
+          placeholder="Maggi"
+          name="ingredient"
+        />
+        <button>Add Ingredient</button>
+      </form>
+
+      <div className="ingredient-list">
+        <h2>Ingredients List:</h2>
+        <ul>{ingredients}</ul>
+      </div>
+    </main>
+  );
 }
