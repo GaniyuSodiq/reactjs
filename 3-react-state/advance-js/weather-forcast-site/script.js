2167708c6cbdc2521c80f4570ab2058b4c4595d6
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2026-02-20 18:28:39 +0100

    write the logic for the time and date

---
+++ 3-react-state/advance-js/weather-forcast-site/script.js
@@ -0,0 +1,92 @@
+
+// GET ALL THE CHANGING PARTS OF THE PAGE
+
+const hourAndMinuteTag = document.getElementById("time-hour-minute")
+const amOrpmTag = document.getElementById("am-or-pm")
+
+const dayAndMonth = document.getElementById("day-and-month")
+const year = document.getElementById("year")
+
+setInterval(()=>{
+    const time = new Date()
+    // LETS GET THE HOURS AND MINUTES
+    const getHours = time.getHours()
+    const getMinutes = time.getMinutes()
+    const hour = getHours > 12 ? getHours - 12 : getHours
+    const amOrpm = getHours > 12 ? `PM` : `AM`
+    const minute = getMinutes < 9 ? `0${getMinutes}` : getMinutes
+    const hourAndMinute = `${hour}:${minute}`
+    hourAndMinuteTag.textContent = hourAndMinute
+    amOrpmTag.textContent = amOrpm
+    console.log(hourAndMinute + amOrpm)
+
+    // LETS GET THE MONTH AND DAY
+    const getDay = time.getDate()
+    let getMonth;
+    const getYear = time.getFullYear()
+
+    switch (time.getMonth()) {
+        case 0:
+            getMonth = "Jan"
+            break;
+        case 1:
+            getMonth = "Feb"
+            break;
+        case 2:
+            getMonth = "Mar"
+            break;   
+        case 3:
+            getMonth = "Apr"
+            break;
+        case 4:
+            getMonth = "May"
+            break;
+        case 5:
+            getMonth = "Jun"
+            break;
+        case 6:
+            getMonth = "Jul"
+            break;
+        case 7:
+            getMonth = "Aug"
+            break;
+        case 8:
+            getMonth = "Sep"
+            break;
+        case 9:
+            getMonth = "Oct"
+            break;
+        case 10:
+            getMonth = "Nov"
+            break;
+        case 11:
+            getMonth = "Dec"
+            break; 
+        default:
+            break;
+    }
+
+    dayAndMonth.textContent = `${getDay} ${getMonth}`
+    year.textContent = getYear
+
+    console.log(`${getDay} ${getMonth} ${getYear}`)
+}, 1000)
+
+
+    // G5NHULAYP3G5EFWZLZY32WER3
+    // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=YOUR_API_KEY
+    // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=11111111111111111
+// Remember that when you see a weather forecast on TV, in a newspaper or on the radio, that anything from 20 degrees upwards is going to be warm, above 25 degrees is hot, above 30 degrees is very hot. Below 20 is cool, below 10 degrees is cold, and below zero degrees means that it will be icy outside as the water will freeze and it will feel very cold outside.
+
+
+async function getWeatherData(city) {
+    try {
+        const url = `url-${city}-${apikey}`
+        const response = await fetch(url)
+        const weatherData = await response.json()
+        // START USING WEATHER DATA
+
+    } catch (error) {
+        // print the error on screen
+    }
+}
